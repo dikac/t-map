837d9057cb9e4c741bf0ebc76b30cca757c568b5
dikac <rigel234@yahoo.com>
2019-11-23 09:45:31 +0700

    swap generic from-iterable-callback

--- dist/from-iterable-callback.d.ts
+++ dist/from-iterable-callback.d.ts
@@ -1,1 +1,1 @@
-export default function FromIterableCallback<Value, Key>(iterable: Iterable<Value>, callback: (value: Value) => Key, map?: Map<Key, Value>): Map<Key, Value>;
+export default function FromIterableCallback<Key, Value>(iterable: Iterable<Value>, callback: (value: Value) => Key, map?: Map<Key, Value>): Map<Key, Value>;

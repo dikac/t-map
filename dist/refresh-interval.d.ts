2b39228817b1c04a032e5d376cfec48deb4254c8
dikac <rigel234@yahoo.com>
2020-11-09 21:41:23 +0700

    add start & stop for refresh-interval

--- dist/refresh-interval.d.ts
+++ dist/refresh-interval.d.ts
@@ -1,8 +1,10 @@
 export default class RefreshInterval<Key, Value> extends Map<Key, Value> {
     #private;
     callback: (set: RefreshInterval<Key, Value>) => void;
-    private interval;
+    private interval?;
     constructor(milliseconds: number, callback: (set: RefreshInterval<Key, Value>) => void, values?: readonly [Key, Value][]);
+    stop(): void;
+    start(): void;
     get seconds(): number;
     set seconds(second: number);
     get milliseconds(): number;
